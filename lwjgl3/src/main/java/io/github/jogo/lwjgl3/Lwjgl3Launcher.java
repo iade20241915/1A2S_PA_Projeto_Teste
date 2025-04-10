d81d3751e032b9c835595ae1d538e5bba12de24d
iade20241915 <20241915@iade.pt>
2025-04-10 17:31:43 +0100

    jogo begining

--- lwjgl3/src/main/java/io/github/jogo/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/jogo/lwjgl3/Lwjgl3Launcher.java
@@ -1,8 +1,8 @@
-package io.github.some_example_name.lwjgl3;
+package io.github.jogo.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.some_example_name.Main;
+import io.github.jogo.Main;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -17,7 +17,7 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("Projeto_1A2S_PA");
+        configuration.setTitle("Dungeon com Obstáculos");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
@@ -27,10 +27,8 @@ public class Lwjgl3Launcher {
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(800, 600);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
